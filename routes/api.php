19599ea541ad445fae3813da1ebef160da3e6321
wisnuben <wisnuarif071@gmail.com>
2024-10-24 16:49:27 +0700

    pembuatan controler analisis lahan

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnalisisLahanController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/analisis-lahan', [AnalisisLahanController::class, 'index']); 
+Route::post('/analisis-lahan', [AnalisisLahanController::class, 'store']);
+Route::put('/analisis-lahan/{id}', [AnalisisLahanController::class, 'update']); // Mengupdate data
+Route::delete('/analisis-lahan/{id}', [AnalisisLahanController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AnalisisLahanController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/analisis-lahan', [AnalisisLahanController::class, 'store']);
