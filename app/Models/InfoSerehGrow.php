cd69485047b69efef6abf8e6f48ddb88a51fd175
salzxq <ogog939@gmail.com>
2024-11-24 11:15:23 +0700

    tab admin add,edit,show done

---
+++ app/Models/InfoSerehGrow.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InfoSerehGrow extends Model
+{
+    protected $table = "cpc_info_sereh_grow";
+
+    protected $fillable = ["deskripsi_sereh_grow","gambar"];
+}
